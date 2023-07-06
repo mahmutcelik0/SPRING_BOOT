737ff4fb1304b808843094282c958306338fab0e
MAHMUT CELIK <mahmut.382@hotmail.com>
2023-07-06 11:11:53 +0300

    CELIK

---
+++ COURSE-2/employee-project/src/main/java/com/mahmut/celik/employeeproject/config/security/DemoSecurityConfig.java
@@ -0,0 +1,68 @@
+package com.mahmut.celik.employeeproject.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+        //Tell Spring Security to use JDBC authentication with our data source
+    }
+
+
+    //KENDI SECURITY FILTER CHAIN IMIZI OLUSTURDUK - HttpSecurity ye authorization, basic login ve csrf disable verdik ve build ettik
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/v1/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/v1/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/v1/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/v1/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/v1/employees/**").hasRole("ADMIN")
+        );
+
+        //USE BASIC AUTHENTICATION
+        http.httpBasic(Customizer.withDefaults());
+
+        //DISABLE CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    /*
+    * @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                            .username("john")
+                            .password("{noop}test123")
+                            .roles("EMPLOYEE")
+                            .build();
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+    *
+    * */
+}
