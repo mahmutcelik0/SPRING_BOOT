04a7e0a516f08ba34e608f67908926eeb3055970
MAHMUT CELIK <mahmut.382@hotmail.com>
2023-07-12 09:09:22 +0300

    lecture-01

--- COURSE-2/SECURITY/01-initial/src/main/java/com/mahmutcelik/demo/controller/WelcomeController.java
+++ COURSE-2/SECURITY/01-initial/src/main/java/com/mahmutcelik/demo/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.mahmutcelik.demo.controller;
 
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
